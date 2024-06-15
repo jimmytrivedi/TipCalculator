e4bc5ec4b168a97bf267d6de42514bb33d95a7cb
Jimmy Trivedi <info@jimmytrivedi.in>
2024-06-15 15:46:51 +0530

    - Removed unnecessary dependency - Added split by functionality

--- app/src/main/java/jimmytrivedi/in/android/tipcalculator/main/HomeActivity.kt
+++ app/src/main/java/jimmytrivedi/in/android/tipcalculator/main/HomeActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import jimmytrivedi.`in`.android.tipcalculator.R
 import jimmytrivedi.`in`.android.tipcalculator.base.BaseActivity
+import jimmytrivedi.`in`.android.tipcalculator.data.Tip
 import jimmytrivedi.`in`.android.tipcalculator.databinding.ActivityHomeBinding
 import kotlinx.coroutines.launch
 
@@ -17,61 +20,113 @@ class HomeActivity : BaseActivity() {
     private val viewModel: HomeActivityViewModel by viewModels()
     private var totalAmount: Double
     private var tipPercent: Double
+    private var spliBy: Int
 
     init {
         totalAmount = 0.0
         tipPercent = 0.0
+        spliBy = 1
     }
 
     override fun init() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.finalAmount.text = getString(R.string.tip_amount_without_colon)
+        initViews()
         setListeners()
     }
 
     override fun initObservers() {
         lifecycleScope.launch {
-            viewModel.finalTip.collect {
-                binding.finalAmount.text = getString(R.string.tip_amount_with_colon, it)
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.totalAmount.collect {
+                    binding.finalAmount.text = getString(R.string.total_amount_with_colon, it)
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.splitAmount.collect {
+                    binding.splitAmount.text = getString(R.string.split_amount_with_colon, it)
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+               viewModel.finalTip.collect {
+                    binding.finalTip.text = getString(R.string.tip_amount_with_colon, it)
+                }
             }
         }
     }
 
-    override fun getBundleParameters(bundle: Bundle) {
+    override fun getBundleParameters(bundle: Bundle) {}
+
+    private fun initViews() {
+        binding.finalAmount.text = getString(R.string.total_amount_without_colon)
+        binding.splitAmount.text = getString(R.string.split_amount_without_colon)
+        binding.finalTip.text = getString(R.string.tip_amount_without_colon)
     }
 
     private fun setListeners() {
+        // Total amount listener
         binding.totalAmount.editText?.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                viewModel.calculateTip(
-                    s.toString(),
-                    binding.tipPercent.editText?.text.toString(),
-                    binding.roundUpToggle.isChecked)
+                val tip = Tip()
+                tip.totalAmount = s.toString()
+                tip.tipPercent = binding.tipPercent.editText?.text.toString()
+                tip.splitBy = binding.splitBy.editText?.text.toString()
+                tip.roundUp = binding.roundUpToggle.isChecked
+
+                viewModel.calculateTip(tip)
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         })
 
+        // Tip percent listener
         binding.tipPercent.editText?.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                viewModel.calculateTip(
-                    binding.totalAmount.editText?.text.toString(),
-                    s.toString(),
-                    binding.roundUpToggle.isChecked)
+                val tip = Tip()
+                tip.totalAmount = binding.totalAmount.editText?.text.toString()
+                tip.tipPercent = s.toString()
+                tip.splitBy = binding.splitBy.editText?.text.toString()
+                tip.roundUp = binding.roundUpToggle.isChecked
+
+                viewModel.calculateTip(tip)
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         })
 
+        // Split by listener
+        binding.splitBy.editText?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val tip = Tip()
+                tip.totalAmount = binding.totalAmount.editText?.text.toString()
+                tip.tipPercent = binding.tipPercent.editText?.text.toString()
+                tip.splitBy = s.toString()
+                tip.roundUp = binding.roundUpToggle.isChecked
+
+                viewModel.calculateTip(tip)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        // Round up listener
         binding.roundUpToggle.setOnCheckedChangeListener { _, isChecked ->
-            viewModel.calculateTip(
-                binding.totalAmount.editText?.text.toString(),
-                binding.tipPercent.editText?.text.toString(),
-                isChecked
-            )
+            val tip = Tip()
+            tip.totalAmount = binding.totalAmount.editText?.text.toString()
+            tip.tipPercent = binding.tipPercent.editText?.text.toString()
+            tip.splitBy = binding.splitBy.editText?.text.toString()
+            tip.roundUp = isChecked
+
+            viewModel.calculateTip(tip)
         }
     }
 }
--- app/src/main/java/jimmytrivedi/in/android/tipcalculator/main/HomeActivityViewModel.kt
+++ app/src/main/java/jimmytrivedi/in/android/tipcalculator/main/HomeActivityViewModel.kt
@@ -3,6 +3,7 @@ package jimmytrivedi.`in`.android.tipcalculator.main
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jimmytrivedi.`in`.android.tipcalculator.base.BaseViewModel
+import jimmytrivedi.`in`.android.tipcalculator.data.Tip
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
@@ -12,19 +13,47 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeActivityViewModel @Inject constructor(): BaseViewModel() {
+    private val _totalAmount = MutableSharedFlow <String>()
+    val totalAmount = _totalAmount.asSharedFlow()
+
+    private val _splitAmount = MutableSharedFlow <String>()
+    val splitAmount = _splitAmount.asSharedFlow()
+
     private val _finalTip = MutableSharedFlow <String>()
     val finalTip = _finalTip.asSharedFlow()
 
-    fun calculateTip(totalAmount: String?, tipPercent: String?, roundUp: Boolean) {
+    fun calculateTip(tip: Tip) {
         viewModelScope.launch {
-            val finalTotalAmount = totalAmount?.toDoubleOrNull() ?: 0.0
-            val finalTipPercent = tipPercent?.toDoubleOrNull() ?: 0.0
+            val finalTotalAmount = tip.totalAmount?.toDoubleOrNull() ?: 0.0
+            val finalTipPercent = tip.tipPercent?.toDoubleOrNull() ?: 0.0
+            var splitBy = 1
 
-            var tip = finalTipPercent / 100 * finalTotalAmount
-            if (roundUp) {
-                tip = kotlin.math.ceil(tip)
+            if (tip.splitBy?.isNotEmpty() == true) {
+                val mSplitBy = tip.splitBy?.toInt() ?: 1
+                if (mSplitBy > 0) {
+                    splitBy = mSplitBy
+                }
             }
-            _finalTip.emit(NumberFormat.getCurrencyInstance(Locale("en", "in")).format(tip))
+
+            val roundUp = tip.roundUp
+
+            if (finalTotalAmount > 0 && finalTipPercent > 0) {
+
+                // Tip calculation
+                var mTip = finalTipPercent / 100 * finalTotalAmount
+                if (roundUp) {
+                    mTip = kotlin.math.ceil(mTip)
+                }
+                _finalTip.emit(NumberFormat.getCurrencyInstance(Locale("en", "in")).format(mTip))
+
+                // Total amount calculation
+                val mAmount = finalTotalAmount - mTip
+                _totalAmount.emit(NumberFormat.getCurrencyInstance(Locale("en", "in")).format(mAmount))
+
+                // Split amount calculation
+                val mSplitAmount = mAmount / splitBy
+                _splitAmount.emit(NumberFormat.getCurrencyInstance(Locale("en", "in")).format(mSplitAmount))
+            }
         }
     }
 }
